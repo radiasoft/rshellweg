13c8885f3619eabe60e53c8d017a4528e291d19a
Pepe Barbe <pepe@radiasoft.net>
2016-08-20 14:11:50 -0500

    TIniFile implementation

--- src/libHellweg2D/IniFiles.cpp
+++ src/libHellweg2D/IniFiles.cpp
@@ -1,12 +1,18 @@
-//#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
 
 #include "IniFiles.hpp"
 
+TIniFile::TIniFile(AnsiString filepath) {
+    pt::ini_parser::read_ini(filepath.c_str(), tree);
+}
+
 long TIniFile::ReadInteger(const AnsiString section, const AnsiString ident, long d) {
-    return 0;
+    const char* locator = (section + "." + ident).c_str();
+    return tree.get<long>(locator);
 }
 
 double TIniFile::ReadFloat(const AnsiString section, const AnsiString ident, double d) {
-    return 0;
+    const char* locator = (section + "." + ident).c_str();
+    return tree.get<double>(locator);
 }
 
--- src/libHellweg2D/IniFiles.hpp
+++ src/libHellweg2D/IniFiles.hpp
@@ -1,12 +1,16 @@
 #ifndef _IniFiles_hpp_
 #define _IniFiles_hpp_
 
+#include <boost/property_tree/ptree.hpp>
+
 #include "AnsiString.hpp"
 
+namespace pt = boost::property_tree;
+
 class TIniFile {
+    pt::ptree tree;
     public:
-        // TODO: Implementation
-        TIniFile(AnsiString filepath) {};
+        TIniFile(AnsiString filepath);
         long ReadInteger(const AnsiString section, const AnsiString ident, long d);
         double ReadFloat(const AnsiString section, const AnsiString ident, double d);
 };
