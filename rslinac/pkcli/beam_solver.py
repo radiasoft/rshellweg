2b3fb0a91a37467f6f502d955e21929b1425dc4f
Pepe Barbe <pepe@radiasoft.net>
2016-09-08 16:44:38 -0500

    More tests and docstrings

--- rslinac/pkcli/beam_solver.py
+++ rslinac/pkcli/beam_solver.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
+"""rslinac beam solver
 """
-"""
+from pyhellweg import PyHellwegCppException
+
+import os
 import pyhellweg
 import sys
 
 
 def run(ini, input, output):
-    """runs the HellWeg2D Beam Solver"""
+    """run beam solver
+
+    Read the input files, execute the beam solver and write the output to the provided destination.
+
+    Args:
+        ini (:obj:`str): path to configuration settings in INI format.
+        input (:obj:`str): path to input data.
+        output (:obj:`str): path to write output data.
+
+    Raises:
+        IOError: if `ini` or `input` cannot be open.
+        RuntimeError: if `output` file already exists.
+        PyHellwegCppException: if an error occurrs within the Beam Solver.
+
+    Todo:
+        * Provide links that document the input formats.
+    """
+
+    # Test if input files are accessible
+    with open(ini, 'r'), open(input, 'r'):
+        pass
+
+    if os.path.exists(output):
+        raise RuntimeError('Output path exists: {}'.format(output))
 
     if sys.version_info >= (3, 0):
         ini = ini.encode()
--- tests/run_beam_solver/beam_solver_test.py
+++ tests/run_beam_solver/beam_solver_test.py
@@ -6,18 +6,25 @@ Contains all tests to ensure that the beam solver is functioning properly.
 Attributes:
     FIXTURE_DIR (:obj:`py.path.local`): contains the base directory where fixtures
     for these tests are stored.
+    INI (:obj:`py.path.local`): INI configuration from the default example
+    INPUT (:obj:`py.path.local`): input file from the default example
+
 
 Todo:
     * Test correctness of the beam solver output
 """
 import os
+import contextlib
 import py
+import pytest
 import rslinac.pkcli.beam_solver
 
 _DIR = os.path.dirname(os.path.realpath(__file__))
 
 FIXTURE_DIR = py.path.local(_DIR)/'test_files'
 
+INI = FIXTURE_DIR/'rslinac.ini'
+INPUT = FIXTURE_DIR/'input.txt'
 
 def test_beam_solver_runs(tmpdir):
     """run beam solver
@@ -26,17 +33,45 @@ def test_beam_solver_runs(tmpdir):
     execute a calculation without crashing.
  
     Args:
-        tmpdir (:obj:`py.path.local`): py.test tmpdir fixture, provides a temporary directory where
+        tmpdir (:obj:`py.path.local`): py.test tmpdir_ fixture, provides a temporary directory where
         outputfiles can be written.
+
+    .. _tmpdir: http://doc.pytest.org/en/latest/tmpdir.html
     """
-    ini = FIXTURE_DIR/'rslinac.ini'
-    _input = FIXTURE_DIR/'input.txt'
     output = tmpdir/'output.txt'
 
-    assert ini.exists()
-    assert _input.exists()
+    assert INI.exists()
+    assert INPUT.exists()
     assert not output.exists()
 
-    rslinac.pkcli.beam_solver.run(str(ini), str(_input), str(output))
+    rslinac.pkcli.beam_solver.run(str(INI), str(INPUT), str(output))
 
     assert output.exists()
+
+def test_io_exception(tmpdir):
+    """io error exceptions
+
+    Test that exceptions are triggered when the I/O parameters specified are erroneous
+
+    Args:
+        tmpdir (:obj:`py.path.local`): py.test tmpdir_ fixture, provides a temporary directory where
+        outputfiles can be written.
+
+    .. _tmpdir: http://doc.pytest.org/en/latest/tmpdir.html
+    """
+    output = tmpdir/'output.txt'
+
+    with pytest.raises(IOError):
+        rslinac.pkcli.beam_solver.run('', str(INPUT), str(output))
+    assert not output.exists()
+
+    with pytest.raises(IOError):
+        rslinac.pkcli.beam_solver.run(str(INI), '', str(output))
+    assert not output.exists()
+
+    with contextlib.closing(output.open('w')):
+        pass
+
+    assert output.exists()
+    with pytest.raises(RuntimeError):
+        rslinac.pkcli.beam_solver.run(str(INI), str(INPUT), str(output))
