445d3df42579b2e3f5831b6649e2f95f920dff44
robnagler <github@q33.us>
2016-09-09 04:07:07 +0000

    feedback on style and testing

--- rslinac/pkcli/beam_solver.py
+++ rslinac/pkcli/beam_solver.py
@@ -1,42 +1,75 @@
 # -*- coding: utf-8 -*-
-"""rslinac beam solver
-"""
-from pyhellweg import PyHellwegCppException
+u"""Run the beam solver library
 
+:copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+#rjn we always put this in the top. It's part of the template:
+#rjn https://github.com/biviosoftware/home-env/blob/master/emacs.d/b-python.el#L102
+from __future__ import absolute_import, division, print_function
+#rjn No vertical space in the import list. No import is really more important
+#rjn than any other unless there's "a reason"
+#rjn This is unused. Does it need to be imported?
+from pyhellweg import PyHellwegCppException
 import os
 import pyhellweg
+import six
 import sys
 
 
-def run(ini, input, output):
-    """run beam solver
+#rjn "input" is a python word. In emacs it highlights as blue so I
+#rjn recommend input_file or input_name
+def run(ini_file, input_file, output_file):
+    #rjn https://google.github.io/styleguide/pyguide.html#TODO_Comments
+    #rjn Also I dont' think it is useful for the comment to be in the
+    #rjn docstring. It's a TODO and therefore doesn't help the user
+    #rjn to see it with the help
+    # TODO(elventear) Provide links that document the input formats
+    """Call `pyhellweg.run_beam_solver` with inputs and write result.
 
-    Read the input files, execute the beam solver and write the output to the provided destination.
+    #rjn line was too long.
+    Read `ini_file` and `input_file`, execute the beam solver, and write
+    result to `output_file`. Arguments may be `py.path.local` objects
+    or strings.
 
     Args:
-        ini (:obj:`str`): path to configuration settings in INI format.
-        input (:obj:`str`): path to input data.
-        output (:obj:`str`): path to write output data.
+        #rjn The nice thing about google style is that you don't
+        #rjn need much RST. Don't end the descriptions with periods
+        #rjn either since they are not sentences.
+        ini_file (object): path to configuration settings in INI format
+        input_file (object): path to input data
+        output_file (object): path to write the result
 
+    #rjn not a huge fan of "raises" statements, because you don't
+    #rjn really know all the errors that my be raised in this case.
+    #rjn It is open() that raises IOError. Rather I would
+    #rjn just document that files must and must not exist
     Raises:
-        IOError: if `ini` or `input` cannot be open.
-        RuntimeError: if `output` file already exists.
-        PyHellwegCppException: if an error occurrs within the Beam Solver.
-
-    Todo:
-        * Provide links that document the input formats.
+        IOError: if `ini` or `input` cannot be opened
+        ValueError: if `output` file already exists
+        PyHellwegCppException: if an error occurrs within the Beam Solver
     """
-
+    #rjn this comment is unnecessary if the error message is clear
     # Test if input files are accessible
-    with open(ini, 'r'), open(input, 'r'):
+    #rjn I find it incongruent using os.path.exists and open. I see the
+    #rjn purpose and I don't have a better alternative
+    args = []
+    for a in ini_file, input_file, output_file:
+        # Normalize if py.path.local
+        a = str(a)
+        if isinstance(a, six.text_type):
+            a = a.encode()
+        args.append(a)
+    with open(args[0], 'r'), open(args[1], 'r'):
         pass
-
-    if os.path.exists(output):
-        raise RuntimeError('Output path exists: {}'.format(output))
-
-    if sys.version_info >= (3, 0):
-        ini = ini.encode()
-        input = input.encode()
-        output = output.encode()
-
-    pyhellweg.run_beam_solver(ini, input, output)
+    if os.path.exists(args[2]):
+        #rjn Use a more specific exception than RuntimeError. ValueError
+        #rjn indicates the problem is with a value.
+        #rjn Put the object of the error first, followed by the error
+        #rjn This is modeled after Unix: cat: foo: No such file or directory
+        raise ValueError('{}: output path exists'.format(args[2]))
+    #rjn Avoid version tests. It's more robust to do feature tests.
+    #rjn In this case you want to know if you have to decode unicode. For
+    #rjn py2 that's unicode and for py3 it's str. "six" provides this
+    #rjn abstraction
+    pyhellweg.run_beam_solver(*args)
--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@
 :copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-import os
-import sysconfig
-
-from glob import glob
-from setuptools import Extension
-
+#rjn The first import has to be pykern, because of the auto-install.
+#rjn setuptools does a lot of caching. (I will update setup.py.jinja
+#rjn to include this.
 try:
     from pykern import pksetup
 except ImportError:
@@ -17,28 +14,47 @@ except ImportError:
     pip.main(['install', 'pykern'])
     from pykern import pksetup
 
-LIB_DIR = os.path.join('src', 'libHellweg2D')
+#rjn sort imports alphabetically. Emacs has a function "sort-lines" (C-x C-l)
+#rjn which handles this so the froms will come before the imports line. This
+#rjn keeps imports in a logical and manageable order.
+from glob import glob
+from setuptools import Extension
+import os
+import sysconfig
 
-PHYS_DIR = os.path.join('src', 'physics')
+#rjn PEP8 says module level declarations are separated by two lines
+#rjn but we only use one line for variables. Global variables not
+#rjn intended for export should begin with underscore.
+#rjn share 'src'
+_LIB_DIR = os.path.join('src', 'libHellweg2D')
+
+_PHYS_DIR = os.path.join('src', 'physics')
 
 
-def get_src_files(directory, extension):
-    """get list of files in `directory` with `extension`"""
+def _get_src_files(directory, extension):
+    #rjn This comment is sufficient for an internal function.
+    #rjn I do think capitalization is nice.
+    """Get list of files in `directory` with `extension`"""
     return glob(os.path.join(directory, '*.{}'.format(extension)))
 
 
-def get_compile_args():
-    """returns compiler arguments based on the environment"""
-    try:
-        # Has the compiler been overrident with environment variables?
-        compiler = os.environ['CC']
-    except KeyError:
-        # Use Python default compiler
-        compiler = sysconfig.get_config_var('CC')
-
+def _get_compile_args():
+    #rjn Specifying "$CC" is more useful to the reader. I realize this
+    #rjn is pedantic, but the style is used elsewhere.
+    """Returns compiler arguments based on $CC or Python's default CC"""
+    #rjn EAFP is noisy in this case. get() is clearer imiho but arguable
+    compiler = os.environ.get('CC', sysconfig.get_config_var('CC'))
+    #rjn much argument here about spacing, but my argument is that if
+    #rjn vertical whitespace is required then it probably needs to be
+    #rjn a separate function. In this case, the blank line is overkill
+    #rjn due to the simplification above
     # libHellweg2D uses c++11 features
     if 'gcc' in compiler:
         return ['-std=c++11']
+    #rjn in this case I think it is important to have a return,
+    #rjn because I looked at the next line and wondered why there wasn't
+    #rjn a return.
+    return None
 
 
 pksetup.setup(
@@ -60,12 +76,26 @@ pksetup.setup(
         'Topic :: Utilities',
     ],
     ext_modules=[
-        Extension(name='pyhellweg', define_macros=[('RSLINAC', 1)],
-                  include_dirs=[LIB_DIR, PHYS_DIR, '/opt/local/include'],
-                  sources=get_src_files(PHYS_DIR, 'cpp')
-                  + get_src_files(LIB_DIR, 'cpp')
-                  + ['pyhellweg.c'],
-                  extra_compile_args=get_compile_args(),
-                 )
-    ]
+        Extension(
+            #rjn all lines should line up on a 4 character boundary so
+            #rjn they are not dependent on identifier lengths. Global
+            #rjn replacements are much easier.
+            name='pyhellweg',
+            define_macros=[('RSLINAC', 1)],
+            #rjn /opt/local/include should not be hardwired. This must be
+            #rjn available from sysconfig?
+            include_dirs=[_LIB_DIR, _PHYS_DIR, '/opt/local/include'],
+            #rjn We put operators at the end of lines to show that the
+            #rjn lines are continued. Alternative argument is that a line
+            #rjn beginning with an operator is a continuation. This doesn't
+            #rjn work with '+' or '-' which are unary operators. The
+            #rjn continued line is indented by 4
+            sources=_get_src_files(_PHYS_DIR, 'cpp') +
+                _get_src_files(_LIB_DIR, 'cpp') +
+                ['pyhellweg.c'],
+            extra_compile_args=_get_compile_args(),
+        #rjn All lines in lists end in ',' so that you can rearrange the sections
+        #rjn without getting syntax errors.
+        ),
+    ],
 )
