296b3d4f5265ae9b94390b93f6755f5372a7261a
Sergey V Kutsaev <kutsaev@radiabeam.com>
2016-09-29 20:20:06 -0700

    Fixed minor GUI bugs

--- src/gui/ResultsForm.cpp
+++ src/gui/ResultsForm.cpp
@@ -551,7 +551,7 @@ void TResForm::DrawLongtSpace()
 			}
 		}
 	}
-	//BeamChart->ExchangeSeries(1,0);
+	BeamChart->ExchangeSeries(1,0);
 
 }
 //---------------------------------------------------------------------------
