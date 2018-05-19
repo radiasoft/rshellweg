bd0b496a6b51ca3c3c7d10effe8e34b69e884526
robnagler <github@q33.us>
2018-05-19 18:55:45 +0000

    pip no longer exports APIs see https://github.com/radiasoft/pykern/issues/23

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@
 :copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
-try:
-    from pykern import pksetup
-except ImportError:
-    import pip
-    pip.main(['install', 'pykern'])
-    from pykern import pksetup
-
 from glob import glob
 from setuptools import Extension
 import os
