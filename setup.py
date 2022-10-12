6318b0887e3f27196d777bd6e96401c2e4051b78
robnagler <github@q33.us>
2022-10-12 14:43:35 +0000

    Remove makefile and make imports qualified

--- setup.py
+++ setup.py
@@ -5,58 +5,60 @@
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
 from glob import glob
-from setuptools import Extension
+import setuptools
 import os
 import sysconfig
-from pykern import pksetup
-from Cython.Build import cythonize
+import pykern.pksetup
+import Cython.Build
 
-_LIB_DIR = os.path.join('src', 'libHellweg2D')
+_LIB_DIR = os.path.join("src", "libHellweg2D")
 
-_PHYS_DIR = os.path.join('src', 'physics')
+_PHYS_DIR = os.path.join("src", "physics")
 
 
 def _get_src_files(directory, extension):
     """Get list of files in `directory` with `extension`"""
-    return glob(os.path.join(directory, '*.{}'.format(extension)))
+    return glob(os.path.join(directory, "*.{}".format(extension)))
 
 
 def _get_compile_args():
     """Returns compiler arguments based on $CC or Python's default CC"""
-    compiler = os.environ.get('CC', sysconfig.get_config_var('CC'))
+    compiler = os.environ.get("CC", sysconfig.get_config_var("CC"))
     # libHellweg2D uses c++11 features
-    if 'gcc' in compiler:
-        return ['-std=c++11', '-I/home/vagrant/.local/include']
+    if "gcc" in compiler:
+        return ["-std=c++11", "-I/home/vagrant/.local/include"]
     return None
 
 
-pksetup.setup(
-    name='rslinac',
-    author='RadiaSoft LLC',
-    author_email='pip@radiasoft.net',
-    description='description',
-    license='http://www.apache.org/licenses/LICENSE-2.0.html',
-    url='https://github.com/radiasoft/rslinac',
+pykern.pksetup.setup(
+    name="rslinac",
+    author="RadiaSoft LLC",
+    author_email="pip@radiasoft.net",
+    description="description",
+    license="http://www.apache.org/licenses/LICENSE-2.0.html",
+    url="https://github.com/radiasoft/rslinac",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
-    ext_modules=cythonize([
-        Extension(
-            name='rslinac.pyhellweg',
-            define_macros=[('RSLINAC', 1)],
-            include_dirs=[_LIB_DIR, _PHYS_DIR],
-            sources=_get_src_files(_PHYS_DIR, 'cpp') +
-                _get_src_files(_LIB_DIR, 'cpp') +
-                ['pyhellweg.pyx'],
-            extra_compile_args=_get_compile_args(),
-        ),
-    ]),
+    ext_modules=Cython.Build.cythonize(
+        [
+            setuptools.Extension(
+                name="rslinac.pyhellweg",
+                define_macros=[("RSLINAC", 1)],
+                include_dirs=[_LIB_DIR, _PHYS_DIR],
+                sources=_get_src_files(_PHYS_DIR, "cpp")
+                + _get_src_files(_LIB_DIR, "cpp")
+                + ["pyhellweg.pyx"],
+                extra_compile_args=_get_compile_args(),
+            ),
+        ]
+    ),
 )
