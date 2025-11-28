7a4c30ff0228c9cd4cddc76a4bad38bc9e2cda38
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-11-28 22:07:58 +0000

    upgrade to pyproject toml

--- rshellweg/rshellweg_console.py
+++ rshellweg/rshellweg_console.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 """Front-end command line for :mod:`rshellweg`.
 
 See :mod:`pykern.pkcli` for how this module is used.
 
-:copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
-:license: http://www.apache.org/licenses/LICENSE-2.0.html
+:copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
+:license: https://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
 
+import pykern.pkcli
 import sys
-
-from pykern import pkcli
 
 
 def main():
-    return pkcli.main("rshellweg")
+    return pykern.pkcli.main("rshellweg")
 
 
 if __name__ == "__main__":
